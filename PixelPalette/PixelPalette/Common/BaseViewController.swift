f8bc3a36c24305d46d4800af8f35791140fcc72a
Sue Cho <suejeong97@naver.com>
2021-07-28 21:04:58 +0900

    [Chore] Directory Hierarchy
    - Set basic directory hierarchy

--- PixelPalette/PixelPalette/Common/BaseViewController.swift
+++ PixelPalette/PixelPalette/Common/BaseViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BaseViewController.swift
 //  PixelPalette
 //
 //  Created by Sue Cho on 2021/07/28.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
