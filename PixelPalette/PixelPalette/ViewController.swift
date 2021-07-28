1792126008a1e0f1a25bd57f7924257ab7e9c65c
Sue Cho <suejeong97@naver.com>
2021-07-28 21:00:55 +0900

    [Init] Setup Project
    - Create Project
    - Add .gitignore and Podfile

---
+++ PixelPalette/PixelPalette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixelPalette
+//
+//  Created by Sue Cho on 2021/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
