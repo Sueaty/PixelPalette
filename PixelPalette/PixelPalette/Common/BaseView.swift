6d723e96aff0f72deb77d78f06408a33dcee3c0d
Sue Cho <suejeong97@naver.com>
2021-08-23 16:57:57 +0900

    [Feat] Create Default View
    - View that will show on launch (before image is loaded)

---
+++ PixelPalette/PixelPalette/Common/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseView.swift
+//  PixelPalette
+//
+//  Created by Sue Cho on 2021/08/23.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PixelPalette/PixelPalette/Common/DefaultView.swift
@@ -0,0 +1,20 @@
+//
+//  DefaultView.swift
+//  PixelPalette
+//
+//  Created by Sue Cho on 2021/08/23.
+//
+
+import UIKit
+
+class DefaultView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
