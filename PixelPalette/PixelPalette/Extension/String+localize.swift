afd9a050948dcc6a65e0edbbe299096462130535
Sue Cho <suejeong97@naver.com>
2021-10-09 04:10:02 +0900

    [Feat] Localize EditColor View (ENG, KOR)

--- PixelPalette/PixelPalette/Extension/String+localize.swift
+++ PixelPalette/PixelPalette/Extension/String+localize.swift
@@ -8,7 +8,13 @@
 import Foundation
 
 extension String {
+    
     func localize(comment: String = "") -> String {
         return NSLocalizedString(self, comment: comment)
     }
+    
+    func localize(with string: String, comment: String = "") -> String {
+        return String(format: self.localize(comment: comment), string)
+    }
+    
 }
